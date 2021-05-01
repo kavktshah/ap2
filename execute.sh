93f01ce1e942c20a3a1f83d0162a106eaf4f15f5
Kavil Parikh <83012798+kavktshah@users.noreply.github.com>
2021-05-01 22:27:49 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,1 +1,9 @@
-aa
+sudo -i
+
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+
+apt update
+
+apt install anydesk
